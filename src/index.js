1c000e4385aec659705b4cb502116a448de35d18
Szymon Kedra <s5zee@wp.pl>
2022-11-28 18:04:46 +0100

    ThemeProvider added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { GlobalStyle } from "./globalStyle";
 import reportWebVitals from "./reportWebVitals";
-
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {},
+  breakpoints: {
+    mobileMin: 320,
+    mobileMax: 767,
+  },
+  radius: {
+    small: "5px",
+    navigationInput: "33px",
+  },
+  shadow: {
+    mainShadow: "0px 4px 12px rgba(186, 199, 213, 0.5);",
+  },
+};
