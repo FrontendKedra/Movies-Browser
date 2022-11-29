e37e8822c05eca05db942ec9b858bd8847a56ea0
Radosław Jeleń <jelu140@gmail.com>
2022-11-29 15:00:59 +0100

    theme refactored

--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,26 @@
+export const breakpoints = {
+  mobileMin: 320,
+  mobileMax: 767
+};
+
+export const radius = {
+  small: "5px",
+  navigationInput: "33px",
+  navigationLinks: "24px"
+};
+
+export const shadow = {
+  mainShadow: "0px 4px 12px rgba(186, 199, 213, 0.5);"
+};
+
+export const colors = {
+  black: "#18181b",
+  white: "#FFFFFF"
+};
+
 export const theme = {
-  colors: {},
-  breakpoints: {
-    mobileMin: 320,
-    mobileMax: 767,
-  },
-  radius: {
-    small: "5px",
-    navigationInput: "33px",
-  },
-  shadow: {
-    mainShadow: "0px 4px 12px rgba(186, 199, 213, 0.5);",
-  },
+  colors: colors,
+  breakpoints: breakpoints,
+  radius: radius,
+  shadow: shadow
 };
