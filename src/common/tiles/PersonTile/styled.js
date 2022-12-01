1a969cbea72e23594153e50ca31f05a124474d94
Magdalena Zulewska <madziapernach@gmail.com>
2022-12-01 21:33:26 +0100

    styles changed

--- src/common/tiles/PersonTile/styled.js
+++ src/common/tiles/PersonTile/styled.js
@@ -1,11 +1,14 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.lightGrey};
+  max-width: 1368px;
+  margin: 0 auto;
   display: flex;
   flex-wrap: wrap;
   gap: 20px;
-  margin: 20px;
+  margin-top: 24px;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.lightGrey};
 `;
 
 export const Container = styled.div`
@@ -16,6 +19,13 @@ export const Container = styled.div`
   padding: 16px;
   display: flex;
   flex-direction: column;
+  box-shadow: ${({ theme }) => theme.shadow.mainShadow};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    width: 136px;
+    height: 245px;
+    padding: 8px;
+  }
 `;
 
 export const ProfileImage = styled.img`
@@ -26,6 +36,11 @@ export const ProfileImage = styled.img`
   border-radius: ${({ theme }) => theme.radius.small};
   object-position: top;
   object-fit: cover;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    width: 120px;
+    height: 178px;
+  }
 `;
 
 export const Name = styled.p`
@@ -34,4 +49,10 @@ export const Name = styled.p`
   color: ${({ theme }) => theme.colors.black};
   text-align: center;
   margin-bottom: 0;
+  line-height: 28.6px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+    font-size: 14px;
+    line-height: 18.2px;
+  }
 `;
