1d73f420649f3c919ba74320eefb03ed50b11b51
Szymon Kedra <s5zee@wp.pl>
2022-12-04 18:53:30 +0100

    br added

--- src/Components/common/LoadingState/Error/index.js
+++ src/Components/common/LoadingState/Error/index.js
@@ -1,21 +1,21 @@
 import {
-    Container,
-    DangerIcon,
-    ErrorMessage,
-    Header,
-    LinkButton,
-  } from "./styled";
-  
-  export const Error = () => (
-    <Container>
-      <DangerIcon />
-      <Header>Ooops! Something went wrong... </Header>
-      <ErrorMessage>
-        Please check your network connection and try again
-      </ErrorMessage>
-      <LinkButton as="a" href="">
-        Back to home page
-      </LinkButton>
-    </Container>
-  );
-  
+  Container,
+  DangerIcon,
+  ErrorMessage,
+  Header,
+  LinkButton,
+} from "./styled";
+
+export const Error = () => (
+  <Container>
+    <DangerIcon />
+    <Header>Ooops! Something went wrong... </Header>
+    <ErrorMessage>
+      Please check your network connection
+      <br /> and try again
+    </ErrorMessage>
+    <LinkButton as="a" href="">
+      Back to home page
+    </LinkButton>
+  </Container>
+);
