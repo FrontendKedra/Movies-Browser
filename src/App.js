ad947c51b20294442a9a17864ccb6f7a1d9f39ed
Magdalena Zulewska <madziapernach@gmail.com>
2022-12-05 14:58:22 +0100

    removed unnecessary components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import { Header } from "./common/Header";
-import { BigTile } from "./common/tiles/BigTile";
-import { MovieTile } from "./common/tiles/MovieTile";
-import { PersonTile } from "./common/tiles/PersonTile";
-import { MovieList } from "./features/movies/MovieList";
-import { MoviePage } from "./features/movies/MoviePage";
-import { Backdrop } from "./features/movies/MoviePage/Backdrop";
-import { PeoplePage } from "./features/peoplePages/PeoplePage";
 
 function App() {
   return (
     <div>
       <Header />
-      <MoviePage />
-      {/* <Backdrop /> */}
-      {/* <PeoplePage /> */}
-      {/* <BigTile /> */}
-      {/* <MovieList /> */}
-      {/* <PersonTile /> */}
-      {/* <MovieTile /> */}
     </div>
   );
 }
