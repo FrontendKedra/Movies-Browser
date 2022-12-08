450455c83b76c23be59dcafd0a723bdd443c46ec
Radosław Jeleń <jelu140@gmail.com>
2022-12-08 14:59:38 +0100

    changing the component styles

--- src/features/movies/MoviePage/Backdrop/styled.js
+++ src/features/movies/MoviePage/Backdrop/styled.js
@@ -1,66 +1,74 @@
 import styled from "styled-components";
 import { ReactComponent as Star } from "./star.svg";
 
-export const Wrapper = styled.div`
-  width: 100%;
-  height: 770px;
+export const Wrapper = styled.section`
   background-color: ${({ theme }) => theme.colors.darkBlack};
-  color: ${({ theme }) => theme.colors.white};
-  margin-top: 0;
   margin-bottom: 64px;
+  margin-top: 0px;
 `;
 
-export const BackdropContainer = styled.div`
-  width: 1368px;
+export const Container = styled.div`
+  position: relative;
+  max-width: 1380px;
   margin: 0 auto;
+  display: flex;
+  align-items: flex-end;
 `;
 
-export const StyledBackdrop = styled.img`
+export const BlureFrames = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    box-shadow: inset 1em 1em 20px 6px black, inset -1em -1em 20px 6px black;
+`;
+
+export const BackdropImg = styled.img`
   width: 100%;
-  height: 769px;
 `;
 
-export const Details = styled.div`
+export const Content = styled.div`
+  padding: 16px;
   display: flex;
   flex-direction: column;
-  padding: 16px;
   justify-content: flex-end;
-  background-position: center;
-  background-size: 140%;
   position: absolute;
+  color: ${({ theme }) => theme.colors.white};
 `;
 
-export const Title = styled.p`
+export const Title = styled.h3`
   font-size: 64px;
   font-weight: 600;
-  line-height: 76.8px;
+  margin-bottom: 25px;
+  margin-top: 0px;
 `;
 
 export const RatingContainer = styled.div`
+`;
+
+export const Rating = styled.div`
+  margin-bottom: 18px;
   display: flex;
-`;
-
-export const StarIcon = styled(Star)`
-  height: auto;
-  width: 40px;
-`;
-
-export const Rates = styled.span`
-  font-weight: 500;
-  font-size: 30px;
-  line-height: 1.3;
-  margin-bottom: 16px;
-`;
-
-export const MaxRates = styled.span`
-  font-size: 16px;
-  font-weight: 400;
-  line-height: 1.2;
   align-items: center;
 `;
 
-export const Votes = styled.span`
-  font-weight: 400;
+export const StarIcon = styled(Star)`
+  height: 40px;
+  width: 40px;
+  margin-right: 8px;
+`;
+
+export const Rate = styled.p`
+  line-height: 1.3;
   font-size: 16px;
-  line-height: 1.2;
+  margin: 0px;
+`;
+
+export const Vote = styled.span`
+  font-weight: 500;
+  font-size: 30px;
+  margin-right: 7px;
+`;
+
+export const Votes = styled.p`
+  margin: 0px;
 `;
