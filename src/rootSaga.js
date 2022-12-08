9f2fe28a5e2a148ed6e87db2e6e3f50cd8c9a114
Szymon Kedra <s5zee@wp.pl>
2022-12-08 13:49:35 +0100

    new rootSaga added

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,7 +1,14 @@
 import { all } from "redux-saga/effects";
 import { watchFetchPopularMovies } from "./features/movies/popularMoviesSaga";
+import { watchFetchPersonDetailsPage } from "./features/people/personPageDetailsSaga";
+import { watchFetchPersonPage } from "./features/people/PersonPage/personPageSaga";
 import { watchFetchPopularPeople } from "./features/people/popularPeopleSaga";
 
 export default function* rootSaga() {
-  yield all([watchFetchPopularMovies(), watchFetchPopularPeople()]);
+  yield all([
+    watchFetchPopularMovies(),
+    watchFetchPopularPeople(),
+    watchFetchPersonPage(),
+    watchFetchPersonDetailsPage(),
+  ]);
 }
