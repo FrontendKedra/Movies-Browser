c679ec28def236cd54df24e41e270bd1917c33c1
Radosław Jeleń <jelu140@gmail.com>
2022-12-13 13:35:36 +0100

    filling component elements with values from the API

--- src/features/movies/MoviePage/Backdrop/index.js
+++ src/features/movies/MoviePage/Backdrop/index.js
@@ -1,4 +1,6 @@
+import { imageBaseUrl } from "../../../../ApiValue"
 import {
+  BackdropImg,
   BlureFrames,
   Container,
   Content,
@@ -10,21 +12,26 @@ import {
   Votes,
   Wrapper,
 } from "./styled";
-import { BackdropImg } from "./styled";
 
-export const Backdrop = () => {
+export const Backdrop = ({
+  title,
+  rating,
+  votes,
+  backdrop_path,
+}) => {
   return (
     <Wrapper>
       <Container>
         <BlureFrames />
-        <BackdropImg src={"https://image.tmdb.org/t/p/w1280/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg"} alt="" />
+        <BackdropImg  src={`${imageBaseUrl}/w1280${backdrop_path}`}
+        alt={`poster of ${title}`} />
         <Content>
-          <Title>Mulan long title</Title>
+          <Title>{title}</Title>
           <RatingContainer>
             <StarIcon />
-            <Vote>10</Vote>
+            <Vote>{rating}</Vote>
             <Rate>/ 10</Rate>
-            <Votes>135 votes</Votes>
+            <Votes>{votes} votes</Votes>
           </RatingContainer>
         </Content>
       </Container>
