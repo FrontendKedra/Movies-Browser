715b4150ac94e3817c95d986f89629adc00fd80e
Szymon Kedra <s5zee@wp.pl>
2022-12-13 11:33:13 +0100

    Genres view mapped through js logic

--- src/common/tiles/MovieTile/Genre/index.js
+++ src/common/tiles/MovieTile/Genre/index.js
@@ -1,10 +1,19 @@
+import { useSelector } from "react-redux";
+import { selectGenres } from "./genreSlice";
 import { GenreContainer, GenreItem } from "./styled";
 
-export const Genre = () => {
+export const Genre = ({ genre_ids }) => {
+  const genres = useSelector(selectGenres);
+
+  const filteredGenres = genre_ids.map((genre) =>
+    genres.filter(({ id }) => genre === id)
+  );
+
   return (
     <GenreContainer>
-      <GenreItem>Action</GenreItem>
-      <GenreItem>Adventure</GenreItem>
+      {filteredGenres.map((genre) =>
+        genre.map(({ name }) => <GenreItem>{name}</GenreItem>)
+      )}
     </GenreContainer>
   );
 };
