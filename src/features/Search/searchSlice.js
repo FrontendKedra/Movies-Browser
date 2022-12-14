f96ec56b35b4cd0bebb1a7fb0551b5e8a32acd74
Magdalena Zulewska <madziapernach@gmail.com>
2022-12-14 22:12:16 +0100

    searchSlice modified

--- src/features/Search/searchSlice.js
+++ src/features/Search/searchSlice.js
@@ -5,26 +5,36 @@ export const searchSlice = createSlice({
   initialState: {
     status: "loading",
     movies: [],
+    query: "",
+    totalResults: 0,
   },
   reducers: {
     fetchSearch: () => ({}),
     fetchSearchSuccess: (state, { payload: movies }) => ({
       status: "succes",
       movies: movies.results,
+      totalResults: movies.total_results,
     }),
     fetchSearchError: () => ({
       status: "error",
       movies: [],
+      totalResults: 0,
     }),
+    setQuery: (state, { payload: query }) => {
+      state.query = query;
+    },
   },
 });
 
-export const { fetchSearch, fetchSearchSuccess, fetchSearchError } =
+export const { fetchSearch, fetchSearchSuccess, fetchSearchError, setQuery } =
   searchSlice.actions;
 
 export const selectSearchState = (state) => state.search;
 
 export const selectSearchMovies = (state) => selectSearchState(state).movies;
 export const selectSearchStatus = (state) => selectSearchState(state).status;
+export const selectSearchQuery = (state) => selectSearchState(state).query;
+export const selectSearchTotalResults = (state) =>
+  selectSearchState(state).totalResults;
 
 export default searchSlice.reducer;
