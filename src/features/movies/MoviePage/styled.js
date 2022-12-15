0ac0fcba31a8cccae5d9869fbc6d0aba189d3eeb
Radosław Jeleń <jelu140@gmail.com>
2022-12-15 08:41:56 +0100

    changes from flex to grid and adjusting the other styles

--- src/features/movies/MoviePage/styled.js
+++ src/features/movies/MoviePage/styled.js
@@ -4,24 +4,32 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   max-width: 1368px;
   margin: 0 auto;
-  display: flex;
-  flex-direction: column;
   background-color: ${({ theme }) => theme.colors.lightGrey};
+  
   @media (max-width: ${({ theme }) => theme.breakpoints.largest}) {
     padding: 16px;
   }
 `;
 
 export const ContentContainer = styled.div`
-  display: flex;
-  gap: 24px;
-  flex-wrap: wrap;
   margin: 0 0 56px 0;
-  @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+  display: grid;
+  grid-template-columns: repeat(6, 1fr); 
+  grid-gap: 24px;
+
+  @media (max-width: 1280px /* ${({ theme }) => theme.breakpoints.extraLarge} */) {
+    grid-template-columns: repeat(5, 1fr);
   }
-  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
-    gap: 16px;
+  @media (max-width: 992px /* ${({ theme }) => theme.breakpoints.larger} */) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+  @media (max-width: 767px /* ${({ theme }) => theme.breakpoints.medium} */) {
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 16px;
+  }
+  @media (max-width: 480px /* ${({ theme }) => theme.breakpoints.small} */) {
     margin: 0 0 21px 0;
+    grid-template-columns: 1fr 1fr;
   }
 `;
 
--- src/features/people/PeoplePage/styled.js
+++ src/features/people/PeoplePage/styled.js
@@ -4,8 +4,6 @@ import { Link } from "react-router-dom";
 export const Wrapper = styled.div`
   max-width: 1368px;
   margin: 0 auto;
-  display: flex;
-  flex-direction: column;
   background-color: ${({ theme }) => theme.colors.lightGrey};
 
   @media (max-width: ${({ theme }) => theme.breakpoints.largest}) {
@@ -14,26 +12,41 @@ export const Wrapper = styled.div`
 `;
 
 export const PersonContainer = styled.div`
-  margin: 0 auto;
-  display: flex;
-  flex-wrap: wrap;
-  gap: 20px;
-  margin-top: 24px;
-  justify-content: center;
+  margin: 0 0 56px 0;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr); 
+  grid-gap: 24px;
+
+  @media (max-width: 1280px /* ${({ theme }) => theme.breakpoints.extraLarge} */) {
+    grid-template-columns: repeat(5, 1fr);
+  }
+  @media (max-width: 992px /* ${({ theme }) => theme.breakpoints.larger} */) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+  @media (max-width: 767px /* ${({ theme }) => theme.breakpoints.medium} */) {
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 16px;
+  }
+  @media (max-width: 480px /* ${({ theme }) => theme.breakpoints.small} */) {
+    margin: 0 0 21px 0;
+    grid-template-columns: 1fr 1fr;
+  }
 `;
 
 export const ListTitle = styled.h1`
-  margin: 56px 0 0;
+  margin: 56px 0px 34px 0px; 
   font-size: 36px;
   font-weight: 600;
   line-height: 1.2;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}) {
+  @media (max-width: 980px /* ${({ theme }) => theme.breakpoints.mobileMax} */) {
     font-size: 24px;
+    margin: 40px 0px 27px 0px;
+    
   }
   @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
     font-size: 18px;
-    margin: 24px 0 0;
+    margin: 24px 0px 20px 0px;
   }
 `;
 
