5e8fd2834b8527c6e4a7ba2cbbb3d7d5a3265d2b
Szymon Kedra <s5zee@wp.pl>
2022-12-15 16:16:25 +0100

    personTile conditions added

--- src/common/tiles/PersonTile/index.js
+++ src/common/tiles/PersonTile/index.js
@@ -1,16 +1,19 @@
 import { imageBaseUrl } from "../../../ApiValue";
 import { Character, Container, Name, ProfileImage } from "./styled";
 import noPicture from "./noPicture.png";
+import { PeopleStyledLink } from "../generisStyles/styled";
 
 export const PersonTile = ({ id, profile_path, name, character }) => {
   return (
-    <Container key={id} id={id}>
-      <ProfileImage
-        src={profile_path ? `${imageBaseUrl}/w342${profile_path}` : noPicture}
-        alt={`portrait of ${name}`}
-      ></ProfileImage>
-      <Name>{name}</Name>
-      <Character>{character}</Character>
-    </Container>
+    <PeopleStyledLink to={`/profile/${id}`} key={id}>
+      <Container>
+        <ProfileImage
+          src={profile_path ? `${imageBaseUrl}/w342${profile_path}` : noPicture}
+          alt={`portrait of ${name}`}
+        ></ProfileImage>
+        {name && <Name>{name}</Name>}
+        {character && <Character>{character}</Character>}
+      </Container>
+    </PeopleStyledLink>
   );
 };
