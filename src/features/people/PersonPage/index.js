ea42e07d6c2e81f530d645165763693f2b770400
Szymon Kedra <s5zee@wp.pl>
2022-12-15 16:20:05 +0100

    fetchGenres, display logic moved to common

--- src/features/people/PersonPage/index.js
+++ src/features/people/PersonPage/index.js
@@ -1,26 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { imageBaseUrl } from "../../../ApiValue";
-import {
-  GenreContainer,
-  GenreItem,
-} from "../../../common/tiles/MovieTile/Genre/styled";
-import {
-  Container,
-  Details,
-  InfoContent,
-  Poster,
-  Title,
-  Year,
-} from "../../../common/tiles/MovieTile/styled";
-import noPoster from "../../../common/tiles/MovieTile/noPoster.png";
-import {
-  Rate,
-  RatingsContainer,
-  StyledStar,
-  Votes,
-} from "../../../common/tiles/MovieTile/Ratings/styled";
 import { fetchPersonDetails, selectPerson } from "../personDetailsSlice";
 import {
   fetchPerson,
@@ -28,28 +8,12 @@ import {
   selectCrew,
   selectPersonStatus,
 } from "./personSlice";
-import {
-  TileContainer,
-  MainContainer,
-  Article,
-  Image,
-  SubHeaderInformation,
-  SubHeaderContainer,
-  BigTileTitle,
-} from "../../../common/tiles/BigTile/styled";
-import {
-  SubHeaderDate,
-  SubHeaderPerson,
-  ContentContainer,
-  Header,
-  Wrapper,
-} from "./styled";
-import noProfile from "../../../common/tiles/PersonTile/noPicture.png";
+import { ContentContainer, Header, Wrapper } from "./styled";
 import { Loader } from "../../../common/states/Loader";
 import { Error } from "../../../common/states/Error";
-
-import { StyledLink } from "../PeoplePage/styled";
-
+import { BigTile } from "../../../common/tiles/BigTile";
+import { MovieTile } from "../../../common/tiles/MovieTile";
+import { fetchGenres } from "../../../common/tiles/MovieTile/Genre/genreSlice";
 
 export const PersonPage = () => {
   const dispatch = useDispatch();
@@ -59,6 +23,7 @@ export const PersonPage = () => {
   useEffect(() => {
     dispatch(fetchPerson(id));
     dispatch(fetchPersonDetails(id));
+    dispatch(fetchGenres());
   }, [dispatch, id]);
 
   const cast = useSelector(selectCast);
@@ -75,35 +40,25 @@ export const PersonPage = () => {
       ) : (
         <Wrapper>
           {person.map(
-            ({ profile_path, name, birthday, place_of_birth, biography }) => (
-              <MainContainer>
-                <Image
-                  src={
-                    profile_path
-                      ? `${imageBaseUrl}/w342${profile_path}`
-                      : noProfile
-                  }
-                  alt={`portrait of ${name}`}
-                />
-                <TileContainer>
-                  <BigTileTitle>{name}</BigTileTitle>
-                  <SubHeaderContainer>
-                    <SubHeaderPerson>
-                      <SubHeaderDate>date of birth:</SubHeaderDate>
-                      <SubHeaderInformation>{birthday}</SubHeaderInformation>
-                      <SubHeaderDate>
-                        {place_of_birth ? "place of birth:" : null}
-                      </SubHeaderDate>
-                      <SubHeaderInformation>
-                        {place_of_birth}
-                      </SubHeaderInformation>
-                    </SubHeaderPerson>
-                  </SubHeaderContainer>
-                </TileContainer>
-                <Article>{biography}</Article>
-              </MainContainer>
+            ({
+              profile_path,
+              name,
+              birthday,
+              place_of_birth,
+              biography,
+              id,
+            }) => (
+              <BigTile
+                title={name}
+                id={id}
+                profile_path={profile_path}
+                article={biography}
+                place_of_birth={place_of_birth}
+                birthday={birthday}
+              />
             )
           )}
+
           {cast.length > 0 && (
             <>
               <Header>
@@ -121,36 +76,18 @@ export const PersonPage = () => {
                     release_date,
                     vote_average,
                     vote_count,
+                    genre_ids,
                   }) => (
-                    <StyledLink to={`/movie-page/${id}`}>
-                      <Container key={id}>
-                        <Poster
-                          src={
-                            poster_path
-                              ? `${imageBaseUrl}/w342${poster_path}`
-                              : noPoster
-                          }
-                          alt={`portrait of ${character}`}
-                        ></Poster>
-                        <InfoContent>
-                          <Details>
-                            <Title>{title}</Title>
-                            <Year>
-                              {character} {character ? release_date : null}
-                            </Year>
-                            <GenreContainer>
-                              <GenreItem>Action</GenreItem>
-                              <GenreItem>Adventure</GenreItem>
-                            </GenreContainer>
-                          </Details>
-                          <RatingsContainer>
-                            <StyledStar />
-                            <Rate>{vote_average}</Rate>
-                            <Votes>{vote_count} votes</Votes>
-                          </RatingsContainer>
-                        </InfoContent>
-                      </Container>
-                    </StyledLink>
+                    <MovieTile
+                      id={id}
+                      title={title}
+                      rating={vote_average}
+                      votes={vote_count}
+                      poster_path={poster_path}
+                      genre_ids={genre_ids}
+                      character={character}
+                      year={release_date}
+                    />
                   )
                 )}
               </ContentContainer>
@@ -174,36 +111,18 @@ export const PersonPage = () => {
                     release_date,
                     poster_path,
                     id,
+                    genre_ids,
                   }) => (
-                    <StyledLink to={`/movie-page/${id}`}>
-                      <Container key={id}>
-                        <Poster
-                          src={
-                            poster_path
-                              ? `${imageBaseUrl}/w342${poster_path}`
-                              : noPoster
-                          }
-                          alt={`portrait of ${job}`}
-                        ></Poster>
-                        <InfoContent>
-                          <Details>
-                            <Title>{title}</Title>
-                            <Year>
-                              {job} {job ? release_date : null}
-                            </Year>
-                            <GenreContainer>
-                              <GenreItem>Action</GenreItem>
-                              <GenreItem>Adventure</GenreItem>
-                            </GenreContainer>
-                          </Details>
-                          <RatingsContainer>
-                            <StyledStar />
-                            <Rate>{vote_average}</Rate>
-                            <Votes>{vote_count} votes</Votes>
-                          </RatingsContainer>
-                        </InfoContent>
-                      </Container>
-                    </StyledLink>
+                    <MovieTile
+                      id={id}
+                      title={title}
+                      rating={vote_average}
+                      votes={vote_count}
+                      job={job}
+                      year={release_date}
+                      poster_path={poster_path}
+                      genre_ids={genre_ids}
+                    />
                   )
                 )}
               </ContentContainer>
