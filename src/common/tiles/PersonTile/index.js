7aa67590ec5bbe71614ce3d85a7290e315e6f8a1
Radosław Jeleń <jelu140@gmail.com>
2022-12-15 08:27:22 +0100

    zmiana struktury komponentu i dodanie parametru "big"

--- src/common/tiles/PersonTile/index.js
+++ src/common/tiles/PersonTile/index.js
@@ -1,16 +1,15 @@
 import { imageBaseUrl } from "../../../ApiValue";
-import { Character, Container, Name, ProfileImage } from "./styled";
+import { Container, ProfileImage, Name, Info } from "./styled";
 import noPicture from "./noPicture.png";
 
-export const PersonTile = ({ id, profile_path, name, character }) => {
+export const PersonTile = ({/*  id, */ profile_path, name, large, character }) => {
   return (
-    <Container key={id} id={id}>
+    <Container /* key={id} id={id} */>
       <ProfileImage
         src={profile_path ? `${imageBaseUrl}/w342${profile_path}` : noPicture}
-        alt={`portrait of ${name}`}
-      ></ProfileImage>
+        alt={`portrait of ${name}`}/> 
       <Name>{name}</Name>
-      <Character>{character}</Character>
+      <Info big={large}>{character}</Info>
     </Container>
   );
 };
