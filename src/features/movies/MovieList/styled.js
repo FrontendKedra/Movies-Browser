a6fdb3603573aaaeaa4e31f56369eedf2e160877
Magdalena Zulewska <madziapernach@gmail.com>
2022-12-16 17:39:02 +0100

    create Pagination Component & used it in MovieList

--- src/features/movies/MovieList/styled.js
+++ src/features/movies/MovieList/styled.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
   max-width: 1368px;
@@ -37,50 +37,4 @@ export const ListTitle = styled.h1`
     font-size: 18px;
     margin: 24px 0 0;
   }
-`;
-
-export const PageChanger = styled.div`
-  display: flex;
-  margin: 40px 0 103px;
-  justify-content: center;
-`;
-
-export const Button = styled.button`
-  display: flex;
-  gap: 8px;
-  align-items: center;
-  padding: 8px 16px;
-  border-radius: 5px;
-  font-size: 14px;
-  line-height: 1.4;
-  background-color: ${({ theme }) => theme.colors.lightBlue};
-  margin-right: 12px;
-  border: none;
-  cursor: pointer;
-
-  ${(props) =>
-    props.disabled &&
-    css`
-      background-color: ${({ theme }) => theme.colors.grey};
-      cursor: not-allowed;
-    `}
-`;
-
-export const PageCounter = styled.div`
-  margin-left: 12px;
-  margin-right: 24px;
-  padding: 8px 16px;
-  justify-content: center;
-  font-size: 16px;
-  font-weight: 600;
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  color: ${({ theme }) => theme.colors.black};
-`;
-
-export const Text = styled.span`
-  font-weight: 400;
-  color: ${({ theme }) => theme.colors.darkGrey};
-
 `;
