c7aabdb15754300d738c1a177508536e57de2fee
Radosław Jeleń <jelu140@gmail.com>
2022-12-16 15:36:33 +0100

    post-conflict error recovery

--- src/common/tiles/PersonTile/index.js
+++ src/common/tiles/PersonTile/index.js
@@ -3,16 +3,17 @@ import { Container, ProfileImage, Name, Info } from "./styled";
 import noPicture from "./noPicture.png";
 import { PeopleStyledLink } from "../generisStyles/styled";
 
-export const PersonTile = ({ id,profile_path, name, big, character }) => {
+export const PersonTile = ({ id, profile_path, name, big, character }) => {
   return (
-    <Container>
-      <PeopleStyledLink to={`/profile/${id}`} key={id} />
-      <ProfileImage
-        src={profile_path ? `${imageBaseUrl}/w342${profile_path}` : noPicture}
-        alt={`portrait of ${name}`}
-      />
-      {name && <Name>{name}</Name>}
-      {character && <Info big={big}>{character}</Info>}
-    </Container>
+    <PeopleStyledLink to={`/profile/${id}`} key={id} >
+      <Container>
+        <ProfileImage
+          src={profile_path ? `${imageBaseUrl}/w342${profile_path}` : noPicture}
+          alt={`portrait of ${name}`}
+        />
+        {name && <Name>{name}</Name>}
+        {character && <Info big={big}>{character}</Info>}
+      </Container>
+    </PeopleStyledLink >
   );
 };
