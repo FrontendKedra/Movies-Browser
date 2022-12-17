019234da137f1996eb8ef254f0ffa77eb3756a29
Magdalena Zulewska <madziapernach@gmail.com>
2022-12-17 14:49:19 +0100

    removed saga and slice from Search components

--- src/features/Search/index.js
+++ src/features/Search/index.js
@@ -1,35 +1,23 @@
-import { useDispatch } from "react-redux";
-import { fetchSearch } from "./searchSlice";
-import { SearchContainer, SearchInput } from "./styled";
+import { SearchInput } from "./styled";
 import useQueryParameter from "../useQueryParameter";
 import { useReplaceQueryParameter } from "../useReplaceQueryParameter";
 
 export const Search = () => {
-  const dispatch = useDispatch();
-
   const query = useQueryParameter("search");
   const replaceQueryParam = useReplaceQueryParameter();
 
   const onInputChange = ({ target }) => {
     replaceQueryParam({
       key: "search",
-      value: target.value.trim(),
+      value: target.value,
     });
   };
 
-  const onFormSubmit = (event) => {
-    event.preventDefault();
-
-    dispatch(fetchSearch(query));
-  };
-
   return (
-    <SearchContainer onSubmit={onFormSubmit}>
-      <SearchInput
-        placeholder="search for movies"
-        value={query || ""}
-        onChange={onInputChange}
-      />
-    </SearchContainer>
+    <SearchInput
+      placeholder="search for movies"
+      value={query || ""}
+      onChange={onInputChange}
+    />
   );
 };
