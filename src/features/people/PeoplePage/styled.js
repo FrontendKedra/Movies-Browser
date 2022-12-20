6ff565f94bdc5203383ef069b8654699fe674913
Szymon Kedra <s5zee@wp.pl>
2022-12-20 17:07:37 +0100

    padding of wrapper changed

--- src/features/people/PeoplePage/styled.js
+++ src/features/people/PeoplePage/styled.js
@@ -2,9 +2,12 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   max-width: 1368px;
-  padding: 16px;
   margin: 0 auto;
   background-color: ${({ theme }) => theme.colors.lightGrey};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.largest}) {
+    padding: 0 16px 16px 16px;
+  }
 `;
 
 export const PersonContainer = styled.div`
