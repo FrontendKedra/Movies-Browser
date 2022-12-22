e0ed2ab8dad61120466d175ffc33ab3e2331457f
Magdalena Zulewska <madziapernach@gmail.com>
2022-12-22 18:09:54 +0100

    Pagination logic changed

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -7,46 +7,58 @@ import {
   PageCounter,
   Span,
 } from "./styled";
+import useQueryParameter from "../../useQueryParameter";
+import { useReplaceQueryParameter } from "../../useReplaceQueryParameter";
 
-export const Pagination = ({ page, setPage, totalPages }) => (
-  <PageChanger>
-    <Button
-      disabled={page === 1}
-      onClick={() => setPage(1)}
-    >
-      <ArrowIconPrevious mobile="true" />
-      <ArrowIconPrevious />
-      <ButtonText>First</ButtonText>
-    </Button>
-    <Button
-      disabled={page === 1}
-      onClick={() => setPage(page - 1)}
-    >
-      <ArrowIconPrevious />
-      <ButtonText>Previous</ButtonText>
-    </Button>
-    <PageCounter>
-      Page
-      <Span>{page}</Span>
-      of
-      <Span>{(totalPages > 500 ? 500 : totalPages)}</Span>
-    </PageCounter>
-    <Button
-      next
-      disabled={page === (totalPages > 500 ? 500 : totalPages)}
-      onClick={() => setPage(page + 1)}
-    >
-      <ButtonText>Next</ButtonText>
-      <ArrowIconNext />
-    </Button>
-    <Button
-      next
-      disabled={page === (totalPages > 500 ? 500 : totalPages)}
-      onClick={() => setPage(totalPages > 500 ? 500 : totalPages)}
-    >
-      <ButtonText>Last</ButtonText>
-      <ArrowIconNext mobile="true" />
-      <ArrowIconNext />
-    </Button>
-  </PageChanger>
-);
+export const Pagination = ({ totalPages }) => {
+  const urlPage = +useQueryParameter("page");
+  const pageChecker = (urlPage) => {
+    return urlPage < 1 ? 1 : urlPage;
+  };
+  const page = pageChecker(urlPage);
+
+  const replaceQueryParameter = useReplaceQueryParameter();
+  const setPage = (page) => {
+    replaceQueryParameter({
+      key: "page",
+      value: page,
+    });
+  };
+
+  return (
+    <PageChanger>
+      <Button disabled={page === 1} onClick={() => setPage(1)}>
+        <ArrowIconPrevious mobile="true" />
+        <ArrowIconPrevious />
+        <ButtonText>First</ButtonText>
+      </Button>
+      <Button disabled={page === 1} onClick={() => setPage(page - 1)}>
+        <ArrowIconPrevious />
+        <ButtonText>Previous</ButtonText>
+      </Button>
+      <PageCounter>
+        Page
+        <Span>{page}</Span>
+        of
+        <Span>{totalPages > 500 ? 500 : totalPages}</Span>
+      </PageCounter>
+      <Button
+        next
+        disabled={page === (totalPages > 500 ? 500 : totalPages)}
+        onClick={() => setPage(page + 1)}
+      >
+        <ButtonText>Next</ButtonText>
+        <ArrowIconNext />
+      </Button>
+      <Button
+        next
+        disabled={page === (totalPages > 500 ? 500 : totalPages)}
+        onClick={() => setPage(totalPages > 500 ? 500 : totalPages)}
+      >
+        <ButtonText>Last</ButtonText>
+        <ArrowIconNext mobile="true" />
+        <ArrowIconNext />
+      </Button>
+    </PageChanger>
+  );
+};
