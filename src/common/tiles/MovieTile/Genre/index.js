29eaed6760b0875f26eb129f36894ec1a027b1f4
Szymon Kedra <s5zee@wp.pl>
2023-01-15 20:20:13 +0100

    genres read from reference, unpacking of an array inside of an array changed to .flat() method

--- src/common/tiles/MovieTile/Genre/index.js
+++ src/common/tiles/MovieTile/Genre/index.js
@@ -1,19 +1,15 @@
-import { useSelector } from "react-redux";
-import { selectGenres } from "./genreSlice";
 import { GenreContainer, GenreItem } from "./styled";
 
-export const Genre = ({ genre_ids }) => {
-  const genres = useSelector(selectGenres);
-
-  const filteredGenres = genre_ids.map((genre) =>
-    genres.filter(({ id }) => genre === id)
-  );
+export const Genre = ({ genre_ids, genres }) => {
+  const filteredGenres = (genre_ids
+    .map((genre) => genres.filter(({ id }) => genre === id))
+  ).flat();
 
   return (
     <GenreContainer>
-      {filteredGenres.map((genre) =>
-        genre.map(({ name }) => <GenreItem key={genre_ids}>{name}</GenreItem>)
-      )}
+      {filteredGenres.map(({ name }) => (
+        <GenreItem key={name}>{name}</GenreItem>
+      ))}
     </GenreContainer>
   );
 };
