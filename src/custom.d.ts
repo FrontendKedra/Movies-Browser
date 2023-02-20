6ab5251bbeb8d29a927747972054ad6463dcad42
Szymon Kedra <s5zee@wp.pl>
2023-02-20 20:05:24 +0100

    custom svg png and jpg typing file created

---
+++ src/custom.d.ts
@@ -0,0 +1,15 @@
+declare module "*.svg" {
+  const value: string;
+  export = value;
+  export const ReactComponent;
+}
+
+declare module "*.png" {
+  const value: string;
+  export = value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export = value;
+}
