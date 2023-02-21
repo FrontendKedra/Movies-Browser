067781cc232e5e59e93cd05533904a8aa8f50d67
Szymon Kedra <s5zee@wp.pl>
2023-02-21 20:52:27 +0100

    movieTupple added to type a destructurized elements in saga

---
+++ src/apiInterfaces/movieInterfaces/movieTupple.ts
@@ -0,0 +1,7 @@
+import { MovieCredits } from "./movieCredits";
+import { MovieDetails } from "./movieDetails";
+
+export type MovieDetailsCreditsTupple = [
+  movieDetails: MovieDetails,
+  movie: MovieCredits
+];
