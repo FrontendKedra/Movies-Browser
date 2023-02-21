48d8d1829b9d2088b2e06632a1a601cd72f75374
Szymon Kedra <s5zee@wp.pl>
2023-02-21 20:51:56 +0100

    movie api endpoints file interfaces added

---
+++ src/apiInterfaces/movieInterfaces/moviePopularAndSearch.ts
@@ -0,0 +1,23 @@
+export interface Result {
+  poster_path: string;
+  adult: boolean;
+  overview: string;
+  release_date: string;
+  genre_ids: number[];
+  id: number;
+  original_title: string;
+  original_language: string;
+  title: string;
+  backdrop_path: string;
+  popularity: number;
+  vote_count: number;
+  video: boolean;
+  vote_average: number;
+}
+
+export interface MoviePopularAndSearch {
+  page: number;
+  results: Result[];
+  total_results: number;
+  total_pages: number;
+}
