3ce50811123bfda0e678ca4fadbf40b226aa892c
Szymon Kedra <s5zee@wp.pl>
2023-02-22 18:08:51 +0100

    movieDetailsSlice typed

---
+++ src/features/movies/MoviePage/movieDetailsSlice.ts
@@ -0,0 +1,58 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../../store";
+import { MovieDetails } from "../../../apiInterfaces/movieInterfaces/movieDetails";
+import {
+  Cast,
+  Crew,
+} from "../../../apiInterfaces/movieInterfaces/movieCredits";
+
+interface MovieDetailsState {
+  status: string;
+  movie: MovieDetails[];
+  cast: Cast[];
+  crew: Crew[];
+}
+
+const initialState: MovieDetailsState = {
+  status: "initial",
+  movie: [],
+  cast: [],
+  crew: [],
+};
+
+const movieDetailsSlice = createSlice({
+  name: "movieDetails",
+  initialState,
+  reducers: {
+    fetchMovieDetails: (state, { payload }) => {
+      state.status = "loading";
+    },
+    fetchMovieDetailsSuccess: (state, { payload: { movieDetails, movie } }) => {
+      state.status = "success";
+      state.movie = [movieDetails];
+      state.cast = movie.cast;
+      state.crew = movie.crew;
+    },
+    fetchMovieDetailsError: (state) => {
+      state.status = "error";
+    },
+  },
+});
+
+export const {
+  fetchMovieDetails,
+  fetchMovieDetailsSuccess,
+  fetchMovieDetailsError,
+} = movieDetailsSlice.actions;
+
+export const selectMovieState = (state: RootState) => state.movieDetails;
+
+export const selectMovieStatus = (state: RootState) =>
+  selectMovieState(state).status;
+export const selectMovie = (state: RootState) => selectMovieState(state).movie;
+export const selectMovieCast = (state: RootState) =>
+  selectMovieState(state).cast;
+export const selectMovieCrew = (state: RootState) =>
+  selectMovieState(state).crew;
+
+export default movieDetailsSlice.reducer;
