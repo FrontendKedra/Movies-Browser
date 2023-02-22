3928a8bb4350f0ff903b166dc0a6421618e605ae
Szymon Kedra <s5zee@wp.pl>
2023-02-22 17:59:39 +0100

    Genre component typed

--- src/common/tiles/MovieTile/Genre/index.tsx
+++ src/common/tiles/MovieTile/Genre/index.tsx
@@ -1,6 +1,12 @@
+import { Genres } from "../../../../apiInterfaces/genres";
 import { GenreContainer, GenreItem } from "./styled";
 
-export const Genre = ({ genre_ids, genres }) => {
+interface GenreProps {
+  genre_ids: number[];
+  genres: Genres[];
+}
+
+export const Genre = ({ genre_ids, genres }: GenreProps) => {
   const filteredGenres = genres.filter(({ id }) => genre_ids.includes(id));
 
   return (
